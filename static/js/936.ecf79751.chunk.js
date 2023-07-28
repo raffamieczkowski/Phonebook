5616e4e5d4c91811586ea81777163927c5e5ffde
raffamieczkowski <108793865+raffamieczkowski@users.noreply.github.com>
2023-07-28 21:10:59 +0000

    Deploying to gh-pages from @ raffamieczkowski/goit-react-hw-08-phonebook@8919d1d5bd8729363fb93394df8e6b81ef685c8b 🚀

---
+++ static/js/936.ecf79751.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[936],{936:function(e,n,h){h.r(n);h(791);var o=h(184);n.default=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Tasks Page"})})}}}]);
+//# sourceMappingURL=936.ecf79751.chunk.js.map
