927b5119e45a2f122b4c97bd05917e0bf97b7cd4
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-08-04 22:47:49 +0200

    Add Helmet Home
    Add Helmet Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Typography from '@mui/material/Typography';
 
 const Home = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-        Welcome to the Phonebook App!
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        Here you can store your contacts and easily manage them.
-      </Typography>
-    </div>
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <div style={{ textAlign: 'center' }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Welcome to the Phonebook App!
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 2 }}>
+          Here you can store your contacts and easily manage them.
+        </Typography>
+      </div>
+    </>
   );
 };
 
