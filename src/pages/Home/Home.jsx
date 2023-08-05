45645bb83eb81054561de9ae599f8c3b1f3382d2
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-08-05 15:26:01 +0200

    update
    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       </Helmet>
       <div style={{ textAlign: 'center', color: '#ffcc33', background: '#000033', borderTop: '1px solid #ffcc33' }}>
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Welcome to the Phonebook App!
+          Phonebook App - enjoy!
         </Typography>
         <Typography variant="body1" sx={{ mb: 2 }}>
           Here you can store your contacts and easily manage them.
